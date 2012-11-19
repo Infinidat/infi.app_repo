731c5c1654d8c1b46ed4858e4cb7a7304fa9f70a
Guy Rozendorn <guy@rzn.co.il>
2012-11-19 14:14:28 +0200

    TRIVIAL removing test

--- tests/test__webserver.py
+++ tests/test__webserver.py
@@ -6,8 +6,3 @@ class WebserverTestCase(TestCase):
         with self.with_webserver_running():
             content = urlopen("http://localhost:8080").read()
             self.assertIn("Available packages", content)
-
-    def test_pull(self):
-        with self.with_new_devlopment_config_file() as configfile:
-            with self.with_webserver_running(configfile):
-                content = urlopen("http://localhost:8080/pull").read()
