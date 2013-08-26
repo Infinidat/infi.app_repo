36648dff0b052e64928daa3964c2447a53ff53f9
Guy Rozendorn <guy@rzn.co.il>
2013-08-26 11:51:29 +0300

    HOSTDEV-1180 fixing logging for workers
    ================================================================================
    
    Host Infrastructure / HOSTDEV-1180
    worker has no effective logging
    
    Type:              Bug                               Status:        Open                       Assignee: Guy Rozendorn
    Priority:          Minor                             Resolution:    Unresolved                 Reporter: Guy Rozendorn
    Affects Version/s:                                   Fix Version/s: 0.34
    Components: app-repo                                                                           Created: 2013-08-26 11:50
    Labels:                                                                                        Updated: 2013-08-26 11:50
    
    Issue Links:
    
    Sub-Tasks:
    
    Description:
    None
    
    Comments:

--- src/infi/app_repo/worker.py
+++ src/infi/app_repo/worker.py
@@ -7,5 +7,11 @@ celery = Celery('infi.app_repo.worker.celery',
                     include=['infi.app_repo.tasks'])
 
 def init(config):
+    import logging
     global celery
     celery.conf.CELERYD_HIJACK_ROOT_LOGGER = False
+    celery.log.get_default_logger().handlers = logging.root.handlers
+    celery.log.setup(loglevel=logging.DEBUG)
+    celery.log.setup_logging_subsystem(loglevel=logging.DEBUG)
+    celery.log.setup_task_loggers(loglevel=logging.DEBUG)
+
