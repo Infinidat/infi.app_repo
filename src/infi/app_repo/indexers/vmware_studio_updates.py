cf84cdc3b902ad6b0b09944189c5ef241c29d37f
Guy Rozendorn <guy@rzn.co.il>
2015-04-25 21:42:09 +0300

    APR-19 extracing zip files into the ova updates repository

--- src/infi/app_repo/indexers/vmware_studio_updates.py
+++ src/infi/app_repo/indexers/vmware_studio_updates.py
@@ -1,6 +1,10 @@
 from .base import Indexer
 from infi.gevent_utils.os import path
-from infi.app_repo.utils import ensure_directory_exists
+from infi.gevent_utils.glob import glob
+from infi.app_repo.filename_parser import parse_filepath, FilenameParsingFailed
+from infi.app_repo.utils import ensure_directory_exists, hard_link_or_raise_exception, log_execute_assert_success
+
+ARCH = "UPDATE_ZIP"
 
 
 class VmwareStudioUpdatesIndexer(Indexer):
@@ -10,11 +14,36 @@ class VmwareStudioUpdatesIndexer(Indexer):
         ensure_directory_exists(self.base_directory)
 
     def are_you_interested_in_file(self, filepath, platform, arch):
+        try:
+            package_name, package_version, platform_string, architecture, extension = parse_filepath(filepath)
+        except FilenameParsingFailed:
+            return False
+        if ARCH in architecture:
+            return True
         return False
 
+    def _get_latest_update_file_in_directory(self, dirpath):
+        from pkg_resources import parse_version
+        latest_update_file, latest_version = None, parse_version('0')
+        update_files = [filepath for filepath in glob(path.join(dirpath, '*.zip')) if ARCH in filepath]
+        for filepath in update_files:
+            package_name, package_version, platform_string, architecture, extension = parse_filepath(filepath)
+            package_version = parse_version(package_version)
+            if package_version > latest_version:
+                latest_version = package_version
+                latest_update_file = filepath
+        return latest_update_file
+
+    def _extract_update(self, dirpath, filepath):
+        log_execute_assert_success(["unzip", "-qq", "-f", filepath, "-d", dirpath])
+
     def consume_file(self, filepath, platform, arch):
-        raise NotImplementedError()
+        package_name, package_version, platform_string, architecture, extension = parse_filepath(filepath)
+        package_dir = path.join(self.base_directory, package_name)
+        ensure_directory_exists(package_dir)
+        final_filepath = hard_link_or_raise_exception(filepath, package_dir)
+        if self._get_latest_update_file_in_directory(package_dir) == final_filepath:
+            self._extract_update(package_dir, final_filepath)
 
     def rebuild_index(self):
         pass
-
---
+++ tests/test_ova_updates.py
@@ -0,0 +1,50 @@
+import unittest
+import mock
+from infi.app_repo.indexers import vmware_studio_updates
+from infi.app_repo import utils
+from infi.gevent_utils.os import path
+from munch import Munch
+
+INDEX = 'main-stable'
+
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        tempdir_context = utils.temporary_directory_context()
+        self.tempdir = tempdir_context.__enter__()
+        self.config = Munch(packages_directory=self.tempdir)
+        self.addCleanup(tempdir_context.__exit__, None, None, None)
+
+    def test_initialise(self):
+        indexer = vmware_studio_updates.VmwareStudioUpdatesIndexer(self.config, INDEX)
+        self.assertFalse(path.exists(indexer.base_directory))
+        indexer.initialise()
+        self.assertTrue(path.exists(indexer.base_directory))
+
+    def test_are_you_interested_in_file(self):
+        indexer = vmware_studio_updates.VmwareStudioUpdatesIndexer(self.config, INDEX)
+        indexer.initialise()
+        self.assertTrue(indexer.are_you_interested_in_file("host-power-tools-for-vmware-1.7.4-vmware-esx-x86_OVF10_UPDATE_ZIP.zip", '', ''))
+
+    def test_consume_file(self):
+        indexer = vmware_studio_updates.VmwareStudioUpdatesIndexer(self.config, INDEX)
+        indexer.initialise()
+        src = 'host-power-tools-for-vmware-1.6.13-vmware-esx-x86_OVF10_UPDATE_ZIP.zip'
+        dst = path.join(indexer.base_directory, 'host-power-tools-for-vmware')
+        utils.write_file(src, '')
+        with mock.patch.object(vmware_studio_updates, 'log_execute_assert_success') as log_execute_assert_success:
+            indexer.consume_file(src, '', '')
+        self.assertTrue(path.exists(path.join(indexer.base_directory, 'host-power-tools-for-vmware', src)))
+        log_execute_assert_success.assert_called_with(['unzip', '-qq', '-f', path.join(dst, src), '-d', dst])
+
+        src = 'host-power-tools-for-vmware-1.7.4-vmware-esx-x86_OVF10_UPDATE_ZIP.zip'
+        utils.write_file(src, '')
+        with mock.patch.object(vmware_studio_updates, 'log_execute_assert_success') as log_execute_assert_success:
+            indexer.consume_file(src, '', '')
+        log_execute_assert_success.assert_called_with(['unzip', '-qq', '-f', path.join(dst, src), '-d', dst])
+
+        src = 'host-power-tools-for-vmware-1.7.3-vmware-esx-x86_OVF10_UPDATE_ZIP.zip'
+        utils.write_file(src, '')
+        with mock.patch.object(vmware_studio_updates, 'log_execute_assert_success') as log_execute_assert_success:
+            indexer.consume_file(src, '', '')
+        log_execute_assert_success.assert_not_called()
+
