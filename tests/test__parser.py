6ce78ae2847b8983bcfb867cadf042a32910cd13
Guy Rozendorn <guy@rzn.co.il>
2015-06-10 09:59:12 +0300

    TRIVIAL adding support for package-x.y.z.bX

--- tests/test__parser.py
+++ tests/test__parser.py
@@ -98,6 +98,9 @@ cases = [
 
     dict(expected=('infinibox-gui', '2.0.0.22', 'custom', 'generic', 'bin'),
          basename="infinibox-gui-2.0.0.22-custom-generic.bin"),
+
+    dict(expected=('infinisdk', '2.0.0.b1', 'python', 'docs', 'tar.gz'),
+         basename="infinisdk-2.0.0.b1-python-docs.tar.gz"),
     ]
 
 class ParserTestCase(TestCase):
